37db382bbaa5a9aa3d8a21bc56848187f52615f4
davit gelashvili <datoogelashvili@gmail.com>
2019-11-14 19:31:17 +0400

    main page
    start: 13:00 (14.11.2019)
    end: 19:00 (11.11.2019)

--- js/main.js
+++ js/main.js
@@ -3,13 +3,9 @@ heroSlider.owlCarousel({
   loop: true,
   items: 1,
   nav: true,
-  dots: true,
-  autoplay: true,
-  animateOut: "slideOutUp",
-  animateIn: "slideInUp",
-  autoplayTimeOut: 5000, //5sec
+  dots: false,
   navText: [
-    '<i class="fa fa-angle-left fa-2x fa-fw" aria-hidden="true"><<</i>',
-    '<i class="fa fa-angle-right fa-2x fa-fw" aria-hidden="true">>></i>'
+    '<i class="fa fa-angle-left fa-2x fa-fw" aria-hidden="true"></i>',
+    '<i class="fa fa-angle-right fa-2x fa-fw" aria-hidden="true"></i>'
   ]
 });
