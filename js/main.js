3dfafb14fe77539c251c14fb4a4281a005378588
davit gelashvili <datoogelashvili@gmail.com>
2019-11-29 18:40:31 +0400

    add

--- js/main.js
+++ js/main.js
@@ -1,11 +1,8 @@
-var heroSlider = $(".header-slider .owl-carousel");
-heroSlider.owlCarousel({
-  loop: true,
-  items: 1,
-  nav: true,
-  dots: false,
-  navText: [
-    '<i class="fa fa-angle-left fa-2x fa-fw" aria-hidden="true"></i>',
-    '<i class="fa fa-angle-right fa-2x fa-fw" aria-hidden="true"></i>'
-  ]
+$(".tender-details-tab-header-item").click(function() {
+  $(".tender-details-tab-header-item").removeClass("active");
+  if (!$(".tender-details-tab-header-item").hasClass("active")) {
+    $(this).addClass("active");
+  } else {
+    $(".tender-details-tab-header-item").removeClass("active");
+  }
 });
