495be76e52276b7d41572b99450b0d0242457350
khimsh <i.khimsh@gmail.com>
2020-02-24 12:28:16 -0800

    add user modal.

--- js/main.js
+++ js/main.js
@@ -37,14 +37,11 @@ $(window).on("load", function() {
 
   // Modal
   $(".modal__close").click(function() {
-    $(".modal").css("display", "none");
+    $(".modal")[0].classList.remove("active");
   });
 
-  // მოდალის გარეთ დაკლიკებაზე დახურვა
-  $(document).click(function(e) {
-    if (!$(e.target).closest(".modal__content").length > 0) {
-      $(".modal").css("display", "none");
-    }
+  $(".add-user-btn").click(function() {
+    $("#add-user-modal")[0].classList.add("active");
   });
 
   $(".header__user").click(function() {
@@ -68,5 +65,11 @@ $(window).on("load", function() {
       "checked",
       this.checked
     );
+  });
+
+  $(".edit").click(function() {
+    this.classList.toggle("active");
+    const editMenu = this.nextElementSibling;
+    editMenu.classList.toggle("active");
   });
 });
